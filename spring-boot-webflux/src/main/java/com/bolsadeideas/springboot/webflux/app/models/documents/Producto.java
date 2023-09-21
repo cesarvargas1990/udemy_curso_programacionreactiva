a3ef0a9adfcd679267c7025c459a40bc852c9443
cesar vargas <cesara.vargas1990@gmail.com>
2023-09-21 10:18:01 -0500

    avances

--- spring-boot-webflux/src/main/java/com/bolsadeideas/springboot/webflux/app/models/documents/Producto.java
+++ spring-boot-webflux/src/main/java/com/bolsadeideas/springboot/webflux/app/models/documents/Producto.java
@@ -2,8 +2,13 @@ package com.bolsadeideas.springboot.webflux.app.models.documents;
 
 import java.util.Date;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Document(collection="productos")
 public class Producto {
@@ -11,17 +16,30 @@ public class Producto {
 	@Id
 	private String id;
 	
+	@NotEmpty
 	private String nombre;
 	
+	@NotNull
 	private Double precio;
 	
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date createAt;
+	
+	@Valid
+	private Categoria categoria;
+	
+	private String foto;
 
 	public Producto() {}
 
 	public Producto(String nombre, Double precio) {
 		this.nombre = nombre;
 		this.precio = precio;
+	}
+	
+	public Producto(String nombre, Double precio, Categoria categoria) {
+		this(nombre, precio);
+		this.categoria = categoria;
 	}
 	
 	public String getId() {
@@ -47,6 +65,22 @@ public class Producto {
 	}
 	public void setCreateAt(Date createAt) {
 		this.createAt = createAt;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
 	}
 	
 	
